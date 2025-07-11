971c79c80f746edf0d7e640ad443970dfb2aa954
Krishnard <krishnadharmale20@gmail.com>
2025-07-11 19:23:55 +0530

    Second commit

--- demo/src/main/java/com/example/demo/EcommerceSpringApplication.java
+++ demo/src/main/java/com/example/demo/EcommerceSpringApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSpringApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.configure().load(); // load .env from root path
+		
+		dotenv.entries().forEach( (DotenvEntry entry ) -> System.setProperty(entry.getKey() , entry.getValue()));
+		// set system properties from environment variables and set them
+		// setting environment variable
+		
 		
 		SpringApplication.run(EcommerceSpringApplication.class, args);
 		
